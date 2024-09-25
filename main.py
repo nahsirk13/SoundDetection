3372a5d55e042d65d7a8949e8668d9d3d05f9624
nahsirk13 <63687682+nahsirk13@users.noreply.github.com>
2024-09-25 18:59:05 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+import wave
+
+def print_hi(name):
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
