4008c4b77fab4005f30a4da3c30e0ba49e3271c0
nahsirk13 <63687682+nahsirk13@users.noreply.github.com>
2024-09-25 21:23:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
 
 import wave
+import matplotlib.pyplot as plt
+import numpy as np
 
-obj = wave.open('Death.wav')
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    obj = wave.open('Death.wav')
+
     print("Number of Channels: ", obj.getnchannels())
     print("Sample width: ", obj.getsampwidth())
     print("Framerate: ", obj.getframerate())
@@ -30,3 +28,21 @@ if __name__ == '__main__':
     obj_wb.setframerate(16000)
     obj_wb.writeframes(frames)
     obj_wb.close()
+
+    obj = wave.open('Death.wav', 'rb') #read binary mode
+    sample_freq = obj.getframerate()
+    n_samples = obj.getnframes()
+    signal_wave = obj.readframes(-1)
+    obj.close()
+    t_audio = n_samples/sample_freq
+    print(t_audio)
+    signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+    times = np.linspace(0, t_audio, num=n_samples)
+
+    plt.figure(figsize=(15, 5))
+    plt.plot(times, signal_array)
+    plt.title("Death Audio Signal")
+    plt.ylabel("Signal wave")
+    plt.xlabel("Time (s)")
+    plt.xlim(0, t_audio)
+    plt.show()
